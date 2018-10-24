508968db83120bd33daba9af7c3080b8e656c6a0
Raudel Garcia <raudel24@gmail.com>
2018-10-23 21:45:08 -0400

    adding candlestick indicator

---
+++ src/main/java/com/garloinvest/gandalf/indicator/oanda/service/impl/FXCandleImpl.java
@@ -0,0 +1,29 @@
+package com.garloinvest.gandalf.indicator.oanda.service.impl;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.garloinvest.gandalf.indicator.oanda.service.FXCandle;
+import com.garloinvest.gandalf.searcher.oanda.dto.OandaInstrumentCandlestick;
+import com.garloinvest.gandalf.searcher.oanda.service.OandaRouter;
+
+@Service
+public class FXCandleImpl implements FXCandle {
+	private static final Logger LOG = LoggerFactory.getLogger(FXCandleImpl.class);
+	
+	@Autowired
+	private OandaRouter router;
+
+	@Override
+	public void readCandlestickData(String instrumentName) {
+		Map<String, Map<LocalDateTime, OandaInstrumentCandlestick>> candlestickData = router.readOandaInstrumentCandlestickPerMinute(instrumentName);
+		LOG.info("Testing Connection with Searcher Service and Thread");
+		LOG.info("Time: {}\nCandleStickData: {}\n Thread: {}",LocalDateTime.now(),candlestickData.size(),Thread.currentThread().getName());
+	}
+
+}
